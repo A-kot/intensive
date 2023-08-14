adf5b00d9f3bba5a997c54f77fb47bf5af2bc30b
A-kot <142221078+A-kot@users.noreply.github.com>
2023-08-14 20:06:25 +0300

    Update int4.py

--- int4.py
+++ int4.py
@@ -1,6 +1,6 @@
 def func(node):
     if node:  # если у вершины есть потомки -> рекурсивно вывести левую ветвь, вершину, правую
         func(node.left)
-        print(node)
+        print(node.val)
         func(node.right)
 
