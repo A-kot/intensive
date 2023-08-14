1ead4be8b9c7dd3525087d94501652c0e5632166
A-kot <142221078+A-kot@users.noreply.github.com>
2023-08-14 18:39:51 +0300

    Update int1.py

--- int1.py
+++ int1.py
@@ -1,7 +1,7 @@
 arr = [9, 5, 8, 1, 2]
 
-for _ in range(len(arr)):
-    for i in range(len(arr)-1):
+for j in range(len(arr)):
+    for i in range(len(arr)-1-j):
         if arr[i] > arr[i+1]:
             tp = arr[i]
             arr[i] = arr[i+1]
