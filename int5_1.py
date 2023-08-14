6fc5439e7fd5e061de6708efe213aebecf0d0218
A-kot <142221078+A-kot@users.noreply.github.com>
2023-08-14 21:06:41 +0300

    Add files via upload

---
+++ int5_1.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def func(queue, visited):
+    if len(queue) > 0:
+        if queue[0] is not None:
+            visited.add(queue[0].value)
+            print(queue[0].value)
+        if queue[0]:
+            queue.append(queue[0].left)
+            queue.append(queue[0].right)
+            queue = queue[1:]
+            func(queue, visited)
+
+
+node = Node(8)
+node.left = Node(5)
+node.right = Node(10)
+node.left.left = Node(3)
+node.left.right = Node(6)
+node.right.left = Node(9)
+node.right.right = Node(11)
+
+visited = set()
+queue = [node]
+print(func(queue, visited))
